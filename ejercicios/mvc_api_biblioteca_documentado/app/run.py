966f39960d75f4fe3198fe6d409cefacd8af52ae
Erick Poma Condori <109001336+as7haro7@users.noreply.github.com>
2024-05-08 17:58:43 +0000

    servidor swangger

--- ejercicios/mvc_api_biblioteca_documentado/app/run.py
+++ ejercicios/mvc_api_biblioteca_documentado/app/run.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from controllers.libro_controller import libro_bp
+from flask_swagger_ui import get_swaggerui_blueprint
 from database import db
 
 app = Flask(__name__)
+
+# Configura la URL de la documentación OpenAPI
+SWAGGER_URL = "/api/docs"  # Ruta para servir Swagger UI
+API_URL = "/static/swagger.json"  # Ruta de tu archivo OpenAPI/Swagger
+# Inicializa el Blueprint de Swagger UI
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "Biblioteca API"}
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 # Configuración de la base de datos
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///biblioteca.db"
