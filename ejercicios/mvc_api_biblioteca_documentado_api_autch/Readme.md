5a98077a20b1200e807874b96743c46ba6b6dc78
as7haro7 <erickbreack72@gmail.com>
2024-05-14 16:58:41 -0400

    ejercicio - api auth y documentacion terminado

---
+++ ejercicios/mvc_api_biblioteca_documentado_api_autch/Readme.md
@@ -0,0 +1,7 @@
+## Crear una API para Gestionar la información de los libros de un biblioteca con las siguientes
+### Características:
+
+● Un libro tiene: Titulo, Autor, Edición y Disponibilidad
+● La API es RESTful
+● La API es un CRUD
+● La API está construida bajo MVC
---
+++ ejercicios/mvc_api_biblioteca_documentado_api_autch/app/run.py
@@ -0,0 +1,42 @@
+from flask import Flask
+# el manager para el proceso de seguirda con tokkens
+from flask_jwt_extended import JWTManager
+from controllers.libro_controller import libro_bp
+from controllers.user_controller import user_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+from database import db
+
+app = Flask(__name__)
+
+# Configuración de la clave secreta para JWT
+app.config["JWT_SECRET_KEY"] = "tu_clave_secreta_aqui"
+# Configura la URL de la documentación OpenAPI
+SWAGGER_URL = "/api/docs"  # Ruta para servir Swagger UI
+API_URL = "/static/swagger.json"  # Ruta de tu archivo OpenAPI/Swagger
+# Inicializa el Blueprint de Swagger UI
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "Biblioteca API"}
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+# Configuración de la base de datos
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///biblioteca.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Inicializa la base de datos
+db.init_app(app)
+
+# Inicializa la extensión JWTManager
+jwt = JWTManager(app)
+
+# Registra el blueprint de libroes en la aplicación
+app.register_blueprint(libro_bp, url_prefix="/api")
+app.register_blueprint(user_bp, url_prefix="/api")
+
+# Crea las tablas si no existen
+with app.app_context():
+    db.create_all()
+
+# Ejecuta la aplicación
+if __name__ == "__main__":
+    app.run(debug=True)
