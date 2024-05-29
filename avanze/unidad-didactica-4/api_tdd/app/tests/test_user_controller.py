b82161930578abdc40b19aeeec3d974239d924b5
Erick Fernando Poma Condori <109001336+as7haro7@users.noreply.github.com>
2024-05-29 18:00:31 +0000

    test user

--- avanze/unidad-didactica-4/api_tdd/app/tests/test_user_controller.py
+++ avanze/unidad-didactica-4/api_tdd/app/tests/test_user_controller.py
@@ -15,3 +15,17 @@ def test_login_user(test_client):
     user_credentials = {"username": "testuser", "password": "testpassword"}
     response = test_client.post("/api/login", json=user_credentials)
     assert response.status_code == 200
+
+
+
+
+# para verificar eror
+def test_login_user_no_data(test_client):
+    user_credentials = {"username": "", "password": ""}
+    response = test_client.post("/api/register", json=user_credentials)
+    assert response.status_code == 400
+
+def test_login_user_fail(test_client):
+    user_credentials = {"username1": "testuser", "password": "testpassword"}
+    response = test_client.post("/api/login", json=user_credentials)
+    assert response.status_code == 401
